63f31524fc4ae30bbf211a703ff408b847ffff5a
Anonymous Person <91095401+Bw11111@users.noreply.github.com>
2022-05-15 16:00:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+import PyInstaller.__ma
+
+from termcolor import colored
+
+
+
+
+try:
+  n = sys.argv[1]
+  if n == "convert":
+    file_name = input('Enter file name: ')
+    with open(file_name) as file_handle:
+      f = open(file_name, "r")
+      f2 = open(file_name + ".py", "w")
+      
+      f2.write("import os \n")
+      f2.write("os.system('''")
+      f2.write(f.read() + "'''" + ')')
+      f2.close()
+      f.close()
+      print("Built python file, use build to build!")
+        
+  if n == "build":
+    file_name = input('Enter file name:')
+    with open(file_name) as file_handle:
+      f3 = open(file_name, "r")
+      PyInstaller.__main__.run([
+    file_name,
+    '--onefile',
+    '--windowed'
+])
+      
+except (RuntimeError, NameError, BaseException): 
+  print( colored('ERROR, NO FILE PASSED!', 'red'))
+  input( colored("PRESS [ENTER] TO CONTINUE...", "red"))
+  pass
